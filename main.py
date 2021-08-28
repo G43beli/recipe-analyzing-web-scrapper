1520df110c63a83bbf07353419799aac563e4257
G43beli <gabrielmassaro@me.com>
2021-08-29 00:06:53 +0200

    added progressbar

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests as req
 import pandas as pd
+import progressbar as pb
 from services.json_service import load_json
-from services.plot_service import plot_bar 
+from services.plot_service import plot_bar
 from bs4 import BeautifulSoup
 from random import randint
 from time import sleep
@@ -13,9 +14,15 @@ page_number_pattern = '{PAGE_NUMBER}'
 url_config = load_json('url_config.json')
 
 data = []
-for region, url in url_config.items():
+
+bar = pb.ProgressBar(maxval=len(url_config.items()), widgets=[pb.Timer(), '  -  ', pb.Percentage(), '  -  ', pb.Bar('#', '[', ']')], redirect_stdout=True)
+bar.start()
+for index, region in enumerate(url_config):
+    bar.update(index) 
+    print(f'[{index}]\tLoading recipes for the following region:', region)
+
+    url = url_config[region]
     url = url.replace(page_number_pattern, '0')
-    print('Loading recipes for the following region:', region, f'({url})')
 
     page = req.get(url, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -27,13 +34,15 @@ for region, url in url_config.items():
 
     recipe_urls = []
     recipes = soup.find_all('article', {'class': 'rsel-item'})
-    
+
     for recipe in recipes:
         recipe_url = recipe.find('a', {'class': 'rsel-recipe'}, href=True)['href']
         recipe_urls.append(recipe_url)
-    
+
     data.append([region, total_recipes, recipe_urls])
-    sleep(randint(100,750) / 1000) # wait between 0.1 and 0.75 seconds before requesting the next page
+    sleep(randint(250,750) / 1000) # wait between 0.25 and 0.75 seconds before requesting the next page
+    
+bar.finish()
 
 df = pd.DataFrame(data, columns = ['Region', 'AmountOfRecipes', 'FirstPageRecipes'])
 plot_bar(df, 'Region', 'AmountOfRecipes', 'Amount of vegan recipes by region (chefkoch.de)', "Region", "Amount of recipes", save_as="graphics/chefkoch_de.png")
